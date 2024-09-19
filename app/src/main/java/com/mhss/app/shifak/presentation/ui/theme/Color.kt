dbc65a33044e52a4826bb92acc00efd5738a05e5
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-09-19 18:25:45 +0300

    Add initial Theme and colors

--- app/src/main/java/com/mhss/app/shifak/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/mhss/app/shifak/presentation/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.mhss.app.shifak.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryColor = Color(0xFF008dda)
+val SecondaryColor = Color(0xFF41C9E2)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightSurfaceColor = Color(0xFFECFFFD)
+val DarkSurfaceColor = Color(0xFF131313)
+
+val LightBackgroundColor = Color(0xFFFFFFFF)
+val DarkBackgroundColor = Color(0xFF000000)
+
+val DarkOnSurfaceColor = Color(0xFF125872)
--- app/src/main/java/com/mhss/app/shifak/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/mhss/app/shifak/presentation/ui/theme/Theme.kt
@@ -9,35 +9,29 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val DarkColorPalette = darkColorScheme(
+    primary = PrimaryColor,
+    surface = DarkSurfaceColor,
+    background = DarkBackgroundColor,
+    surfaceTint = Color.Transparent,
+    surfaceVariant = DarkSurfaceColor
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val LightColorPalette = lightColorScheme(
+    primary = PrimaryColor,
+    surface = LightSurfaceColor,
+    background = LightBackgroundColor,
+    surfaceTint = Color.Transparent,
+    surfaceVariant = LightSurfaceColor
 )
 
 @Composable
 fun ShifakTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    darkTheme: Boolean = false,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -46,8 +40,8 @@ fun ShifakTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
     }
 
     MaterialTheme(
