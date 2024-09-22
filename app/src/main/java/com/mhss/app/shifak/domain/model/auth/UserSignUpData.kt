77bbf55fc9f93e2bb2a4340f18b1ee7f3ab3aa84
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-09-22 12:28:48 +0300

    Edit model classes

--- app/src/main/java/com/mhss/app/shifak/domain/model/auth/UserSignUpData.kt
+++ app/src/main/java/com/mhss/app/shifak/domain/model/auth/UserSignUpData.kt
@@ -1,10 +1,14 @@
 package com.mhss.app.shifak.domain.model.auth
 
+import com.mhss.app.shifak.util.Gender
+
 data class UserSignUpData(
     val fullName: String,
     val phone: String,
     val nationalId: String,
     val email: String,
     val password: String,
-    val passwordConf: String
+    val passwordConf: String,
+    val birthDate: Long,
+    val gender: Gender
 )
--- app/src/main/java/com/mhss/app/shifak/domain/model/drug/DrugType.kt
+++ app/src/main/java/com/mhss/app/shifak/domain/model/drug/DrugType.kt
@@ -5,3 +5,17 @@ data class DrugType(
     val name: String,
     val unit: String
 )
+
+val drugTypesMap = hashMapOf(
+    1 to DrugType(1, "أقراص", "قرص"),
+    2 to DrugType(2, "كبسولات", "كبسولة"),
+    3 to DrugType(3, "شراب", "مل"),
+    4 to DrugType(4, "حقن", "مل"),
+    5 to DrugType(5, "كريم", "جرام"),
+    6 to DrugType(6, "مرهم", "جرام"),
+    7 to DrugType(7, "بخاخ", "رشات"),
+    8 to DrugType(8, "محلول", "مل"),
+    9 to DrugType(9, "قطرات", "قطرة"),
+    10 to DrugType(10, "تحاميل", "تحميلة"),
+    11 to DrugType(11, "لاصقات", "لاصقة")
+)
