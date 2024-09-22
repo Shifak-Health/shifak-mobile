51f7f8834e566165ec055b7fbaf55ff9022bc40e
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-09-22 12:29:53 +0300

    Add DateUtil functions

--- app/src/main/java/com/mhss/app/shifak/util/DateUtil.kt
+++ app/src/main/java/com/mhss/app/shifak/util/DateUtil.kt
@@ -1,2 +1,23 @@
 package com.mhss.app.shifak.util
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaLocalDateTime
+import kotlinx.datetime.toLocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+fun now(): Long {
+    return Clock.System.now().toEpochMilliseconds()
+}
+
+fun Long.formattedForNetwork(): String {
+    val localDateTime = Instant.fromEpochMilliseconds(this).toLocalDateTime(
+        TimeZone.currentSystemDefault()
+    )
+
+    return DateTimeFormatter
+        .ofPattern("yyyy-MM-dd", Locale.ENGLISH)
+        .format(localDateTime.toJavaLocalDateTime())
+}
